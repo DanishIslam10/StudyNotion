e5e0406a942e0ffed4b11a1e4708c07b0e406361
DanTheBest <danishislam328@gmail.com>
2024-11-16 01:07:46 +0530

    Resolved Cors

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
+// Create axios instance with a base URL
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
-    withCredentials: true, // This is important for sending cookies
-})
+    baseURL: process.env.REACT_APP_BACKEND_URL, // Ensure the prefix if using CRA
+    withCredentials: true, // Important for cookies
+});
 
+// Function to make API requests
 export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
         method: method,
         url: url,
         data: bodyData ? bodyData : null,
         headers: headers ? headers : null,
-        params: params ? params : null
-    })
-}
+        params: params ? params : null,
+    });
+};
